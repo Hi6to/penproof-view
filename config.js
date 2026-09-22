1c0a280c9814ed70c47ac60199ebac661f523ecb
Hi6to <82304578+Hi6to@users.noreply.github.com>
2026-09-23 00:02:41 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// PenProof 閲覧ページの設定(実際の値)。
+window.PENPROOF_CONFIG = {
+  SUPABASE_URL: "https://oompgtlkujhmrvmgryaz.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vbXBndGxrdWpobXJ2bWdyeWF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAwNjM2MzIsImV4cCI6MjEwNTYzOTYzMn0.AT7lDkZVzcIbsHFb2jQynaAXVwyEOrdlQKdechjvDPE",
+};
